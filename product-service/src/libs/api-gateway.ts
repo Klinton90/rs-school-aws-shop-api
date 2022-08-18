01d23da149c6e1fb4326e4383abcefca6fd5159d
Klinton90 <alextymofeev@gmail.com>
2022-08-17 21:19:48 -0400

    Task6 - sqs and sns integration

--- product-service/src/libs/api-gateway.ts
+++ product-service/src/libs/api-gateway.ts
@@ -4,21 +4,26 @@ import type { FromSchema } from "json-schema-to-ts";
 type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
 export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
 
-export const formatJSONResponse = (response: any) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(response)
-  }
+interface MyResponse {
+  statusCode: number;
+  body?: string;
 }
 
-export const formatNotFoundError = () => {
+export const formatJSONResponse = (response?: any): MyResponse => {
+  return {
+    statusCode: 200,
+    body: response ? JSON.stringify(response) : '',
+  };
+}
+
+export const formatNotFoundError = (): MyResponse => {
   return {
     statusCode: 404,
     body: ''
   }
 }
 
-export const formatInvalidError = () => {
+export const formatInvalidError = (): MyResponse => {
   return {
     statusCode: 400,
     body: ''
